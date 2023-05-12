f0bedddebb9206a35763704e4f2ca75e2124f665
Mnagol <102668051+Mnagol@users.noreply.github.com>
2023-05-11 21:22:18 -0400

    initialized

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -7,26 +7,28 @@ function filterList(list, query) {
   return list.filter((item) => {
     const lowerCaseName = item.name.toLowerCase();
     const lowerCaseQuery = query.toLowerCase();
-    return lowerCaseName.includes(lowerCaseQuery)
-})
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
 }
+
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const mainForm = document.querySelector('.main_form'); // Initialize mainForm variable
   const filterButton = document.querySelector('.filter_button');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+  mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
     filterButton.addEventListener('click', (event) => {
-      console.log('clicked FilterButton')
+      console.log('clicked FilterButton');
 
-      const formData = new formData(mainForm);
+      const formData = new FormData(mainForm);
       const formProps = Object.fromEntries(formData);
 
       console.log(formProps);
       const newList = filterList(currentList, formProps.resto);
 
       console.log(newList);
-    })
+    });
+
     /*
       ## GET requests and Javascript
         We would like to send our GET request so we can control what we do with the results
@@ -71,14 +73,6 @@ async function mainEvent() { // the async keyword means we can make API requests
     // This changes the response from the GET into data we can use - an "object"
     const arrayFromJson = await results.json();
     console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it initially contains all 1,000 records from your request
-  });
-}
+    // arrayFromJson.data -
 
-/*
-  This adds an event listener that fires our main event only once our page elements have loaded
-  The use of the async keyword means we can "await" events before continuing in our scripts
-  In this case, we load some data when the form has submitted
-*/
-document.addEventListener('DOMContentLoaded', async () => mainEvent());
+document.addEventListener('DOMContentLoaded', async () => mainEvent());})}
